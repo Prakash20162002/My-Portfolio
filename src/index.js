60616c5e1971d78798f362940471496812abf6f2
Prakash <Prakash.com>
2023-10-17 00:17:32 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const LogInCollection = require("./mongo")
+const port = process.env.PORT || 3000
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+const tempelatePath = path.join(__dirname, '../templates')
+const publicPath = path.join(__dirname, '../public')
+console.log(publicPath);
+app.set('view engine', 'hbs')
+app.set('views', tempelatePath)
+app.use(express.static(publicPath))
+
+
+
+app.get('/contact', (req, res) => {
+    res.render('contactus')
+})
+
+
+app.post('/contact', async (req, res) => {
+
+    try {
+
+        const { email } = req.body;
+
+        const validateUser = await LogInCollection.findOne({ email: email })
+
+        if (validateUser) {
+            return res.status(400).json({ error: "Alredy Sended" })
+        }
+
+        const data = await LogInCollection(req.body)
+
+        await data.save()
+
+        res.status(200).redirect('/')
+
+
+    } catch (error) {
+
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+
+})
+
+app.listen(process.env.PORT || port, () => {
+    console.log('port connected');
+})
---
+++ src/mongo.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+// import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://prakashhalwai59:prakash123@cluster0.dlz75bp.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {
+        console.log('connected');
+    })
+    .catch((e) => {
+        console.log(e);
+    })
+
+const logInSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    number:{
+        type: String
+    },
+    Emailsubject: {
+        type: String,
+    }, 
+    Massage:
+    {
+        type: String,
+    }
+})
+
+const LogInCollection = new mongoose.model('users', logInSchema)
+
+module.exports = LogInCollection
