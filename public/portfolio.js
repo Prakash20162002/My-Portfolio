93d6c65fc6a550211a078f60460a78f7be25e15c
Prakash <Prakash.com>
2023-10-19 23:50:15 +0530

    Finally All Completed

--- public/portfolio.js
+++ public/portfolio.js
@@ -2,7 +2,7 @@ let menuIcon = document.querySelector('#menu-icon');
 let nevbar = document.querySelector('.navbar');
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
-    nevbar.classList.toggle('active')
+    nevbar.classList.toggle('active');
 };
 let sections = document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
@@ -26,6 +26,26 @@ window.onscroll=()=>
 let header=document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
     
-
+    menuIcon.classList.remove('bx-x');
+    nevbar.classList.remove('active');
     
 };
+
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+ScrollReveal().reveal('.home-content, .heading', { origin:'top' });
+ScrollReveal().reveal('.home-img, .services-container,.portfolio-box,.contact form', { origin:'bottom' });
+ScrollReveal().reveal('.home-content h1,.about-img', { origin:'left' });
+ScrollReveal().reveal('.home-content p,.about-content', { origin:'right'});
+
+const typed = new Typed('.multipletext', {
+    Strings:['Videographer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
